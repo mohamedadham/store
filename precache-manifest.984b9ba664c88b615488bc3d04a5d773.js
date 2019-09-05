c36a20632582f2b7a96c62730c3147eb129f2dc7
Mohamed <mohamedadham27@gmail.com>
2019-09-05 15:29:42 +0200

    Updates

---
+++ precache-manifest.984b9ba664c88b615488bc3d04a5d773.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102c355750b00bdc30e6488125d428ac",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "14bc67c04fd434b0e0e7",
+    "url": "/store/static/css/main.cd01b98d.chunk.css"
+  },
+  {
+    "revision": "937885eea8cbd936b5d4",
+    "url": "/store/static/js/2.3688c3bf.chunk.js"
+  },
+  {
+    "revision": "14bc67c04fd434b0e0e7",
+    "url": "/store/static/js/main.29f45318.chunk.js"
+  },
+  {
+    "revision": "aa13af5d70743cfce420",
+    "url": "/store/static/js/runtime~main.8bbc46a3.js"
+  }
+]);
